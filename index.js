7d3b9783bbcebb9a1cc9c30b556c305739952ea8
UmutTurk24 <umturk@davidson.edu>
2023-03-31 11:13:49 -0400

    Hehe

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const expressHandlebars = require('express-handlebars').engine;
+const expressHandlebars = require('express-handlebars').engine;
 const unirest = require("unirest"); // For retrieving the geo-location
 const { Navigator } = require("node-navigator");
 const request = require('request');
@@ -16,6 +16,8 @@ const client = new MongoClient(d_url);
 app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main',
 }))
+app.set('view engine', 'handlebars')
+
 
 const weather_data = function (req, res, next) {
     navigator.geolocation.getCurrentPosition((success, error) => {
@@ -25,22 +27,24 @@ const weather_data = function (req, res, next) {
             var longitude = success.longitude;
 
             // Get the current location (city/town name)
-            var request_url = 'https://api-bdc.net/data/reverse-geocode?latitude=' + latitude + '&longitude=' + longitude + '&localityLanguage=en&key=bdc_6d69005a593841fc8312a576ddb14a06';
-            var result = request( request_url, { json: true }, (err, res, body) => {
+            var request_reverse_geolocation_url = 'https://api-bdc.net/data/reverse-geocode?latitude=' + latitude + '&longitude=' + longitude + '&localityLanguage=en&key=bdc_6d69005a593841fc8312a576ddb14a06';
+            var result = request( request_reverse_geolocation_url, { json: true }, (err, res, body) => {
                 if (err) { return console.log(err); }
-                // console.log(body.locality); // get the town
 
                 // Get the weather information in the current location
-                var request_url2 = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude +'&appid=f5495a71a4b6e683120cbc8b11b3b556';
-                var result2 = request( request_url2, { json: true }, (err, res, body1) => {
+                var request_weather_info_url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude +'&appid=f5495a71a4b6e683120cbc8b11b3b556';
+                var result2 = request( request_weather_info_url, { json: true }, (err, res, body1) => {
                     if (err) { return console.log(err); }
-                    // console.log(body1);
-                    const ret_value = [latitude, longitude, body.locality, body1];
-                    // console.log(ret_value);
+
+                    // Organize variables
+                    const weather_description = body1.weather[0].description;
+                    const icon_type = body1.weather[0].icon;
+                    const town_name = body.locality;
+                    const temperature = body1.main.temp;
+
+                    const ret_value = [temperature, town_name, weather_description, icon_type];
                     req.weather_data = ret_value;
                     next();
-                    // return ret_value;
-                    // https://openweathermap.org/img/wn/10d@2x.png // GETTING THE ICON
                 });
             });
         }
@@ -52,7 +56,7 @@ app.use(cookieParser());
 
 app.get('/',  (req, res, next) => {
     const cur_data = req.weather_data;
-    // console.log(cur_data);
+    console.log(cur_data);
 })
 
 app.listen(port, () => {
