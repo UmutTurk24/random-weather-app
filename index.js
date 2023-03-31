7a449ed1b623f65f9d87f334a4d7cc3fe300943a
UmutTurk24 <umturk@davidson.edu>
2023-03-31 11:13:28 -0400

    Revert "Cleared-Main"
    This reverts commit 23e94cc945bba79fe8f25169a260eef0cbc16830.

--- index.js
+++ index.js
@@ -13,45 +13,34 @@ const port = 3000;
 const d_url = 'mongodb+srv://umut1656:mycoolapp@cluster0.8w0gcuf.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(d_url);
 
-// configure Handlebars view engine
-// app.engine('handlebars', expressHandlebars({
-//     defaultLayout: 'main',
-//   }));
-// app.set('view engine', 'handlebars')
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}))
 
 const weather_data = function (req, res, next) {
     navigator.geolocation.getCurrentPosition((success, error) => {
         if (error) console.error(error);
         else{
-            const latitude = success.latitude;
-            const longitude = success.longitude;
+            var latitude = success.latitude;
+            var longitude = success.longitude;
 
             // Get the current location (city/town name)
-            var request_reverse_geolocation_url = 'https://api-bdc.net/data/reverse-geocode?latitude=' + latitude + '&longitude=' + longitude + '&localityLanguage=en&key=bdc_6d69005a593841fc8312a576ddb14a06';
-            var result = request( request_reverse_geolocation_url, { json: true }, (err, res, res_body1) => {
-                // Return if API request failed
+            var request_url = 'https://api-bdc.net/data/reverse-geocode?latitude=' + latitude + '&longitude=' + longitude + '&localityLanguage=en&key=bdc_6d69005a593841fc8312a576ddb14a06';
+            var result = request( request_url, { json: true }, (err, res, body) => {
                 if (err) { return console.log(err); }
+                // console.log(body.locality); // get the town
 
                 // Get the weather information in the current location
-                var request_weather_info_url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude +'&appid=f5495a71a4b6e683120cbc8b11b3b556';
-                var result2 = request( request_weather_info_url, { json: true }, (err, res, res_body2) => {
-                    // Return if API request failed
+                var request_url2 = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude +'&appid=f5495a71a4b6e683120cbc8b11b3b556';
+                var result2 = request( request_url2, { json: true }, (err, res, body1) => {
                     if (err) { return console.log(err); }
-
-                    // Organize variables
-                    const town_name = res_body1.locality;
-                    const icon_name = res_body2.weather[0].icon;
-                    const weather_description = res_body2.weather[0].description;
-
-                    var request_icon = 'https://openweathermap.org/img/wn/' + icon_name + '@2x.png';
-                    var result3 = request( request_weather_info_url, { json: true }, (err, res, res_body3) => { 
-                        if (err) { return console.log(err); }
-                        const icon_pic = res_body3;
-                        // Return the captured data
-                        const ret_value = [latitude, longitude, town_name, icon_name, weather_description, icon_pic];
-                        req.weather_data = ret_value;
-                        next();
-                    });                    
+                    // console.log(body1);
+                    const ret_value = [latitude, longitude, body.locality, body1];
+                    // console.log(ret_value);
+                    req.weather_data = ret_value;
+                    next();
+                    // return ret_value;
+                    // https://openweathermap.org/img/wn/10d@2x.png // GETTING THE ICON
                 });
             });
         }
@@ -63,17 +52,8 @@ app.use(cookieParser());
 
 app.get('/',  (req, res, next) => {
     const cur_data = req.weather_data;
-    console.log(weather_data);
-    // if (req.cookie.hasVisited) {
-    //     res.redirect('/app');
-    // } else {
-    //     res.cookie('hasVisited', 'True');
-    // }
-});
-
-// app.get('/app', (req, res) => {
-//     res.render('about', { fortune: randomFortune });
-// });
+    // console.log(cur_data);
+})
 
 app.listen(port, () => {
     console.log( `Express started on http://localhost:${port}` +
