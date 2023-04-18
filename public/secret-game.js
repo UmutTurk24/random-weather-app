3ba45aa7add514ba33509cb852de1604b79632c5
UmutTurk24 <umturk@davidson.edu>
2023-04-17 21:08:41 -0400

    Snow, snake

---
+++ public/secret-game.js
@@ -0,0 +1,269 @@
+var canvas = document.getElementById('game');
+var context = canvas.getContext('2d');
+
+var shakingElements = [];
+shakingElements.push(canvas);
+
+var grid = 16;
+var count = 0;
+  
+var snake = {
+  x: 160,
+  y: 160,
+  
+  // snake velocity. moves one grid length every frame in either the x or y direction
+  dx: grid,
+  dy: 0,
+  
+  // keep track of all grids the snake body occupies
+  cells: [],
+  
+  // length of the snake. grows when eating an apple
+  maxCells: 4
+};
+var apple = {
+  x: 320,
+  y: 320
+};
+
+// get random whole numbers in a specific range
+// @see https://stackoverflow.com/a/1527820/2124254
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// game loop
+function loop() {
+  requestAnimationFrame(loop);
+
+  // slow game loop to 15 fps instead of 60 (60/15 = 4)
+  if (++count < 4) {
+    return;
+  }
+
+  count = 0;
+  context.clearRect(0,0,canvas.width,canvas.height);
+
+  // move snake by it's velocity
+  snake.x += snake.dx;
+  snake.y += snake.dy;
+
+  // wrap snake position horizontally on edge of screen
+  if (snake.x < 0) {
+    snake.x = canvas.width - grid;
+  }
+  else if (snake.x >= canvas.width) {
+    snake.x = 0;
+  }
+  
+  // wrap snake position vertically on edge of screen
+  if (snake.y < 0) {
+    snake.y = canvas.height - grid;
+  }
+  else if (snake.y >= canvas.height) {
+    snake.y = 0;
+  }
+
+  // keep track of where snake has been. front of the array is always the head
+  snake.cells.unshift({x: snake.x, y: snake.y});
+
+  // remove cells as we move away from them
+  if (snake.cells.length > snake.maxCells) {
+    snake.cells.pop();
+  }
+
+  // draw apple
+  context.fillStyle = 'red';
+  context.fillRect(apple.x, apple.y, grid-1, grid-1);
+
+  // draw snake one cell at a time
+  context.fillStyle = 'green';
+  snake.cells.forEach(function(cell, index) {
+    
+    // drawing 1 px smaller than the grid creates a grid effect in the snake body so you can see how long it is
+    context.fillRect(cell.x, cell.y, grid-1, grid-1);  
+
+    // snake ate apple
+    if (cell.x === apple.x && cell.y === apple.y) {
+      snake.maxCells++;
+
+      // animation for shaking the screen
+      shake(canvas);
+      
+      // canvas is 400x400 which is 25x25 grids 
+      apple.x = getRandomInt(5, 35) * grid;
+      apple.y = getRandomInt(5, 35) * grid;
+    }
+
+    // check collision with all cells after this one (modified bubble sort)
+    for (var i = index + 1; i < snake.cells.length; i++) {
+      
+      // snake occupies same space as a body part. reset game
+      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+        snake.x = 160;
+        snake.y = 160;
+        snake.cells = [];
+        snake.maxCells = 4;
+        snake.dx = grid;
+        snake.dy = 0;
+
+        apple.x = getRandomInt(5, 35) * grid;
+        apple.y = getRandomInt(5, 35) * grid;
+      }
+    }
+  });
+}
+
+// listen to keyboard events to move the snake
+document.addEventListener('keydown', function(e) {
+  // prevent snake from backtracking on itself by checking that it's 
+  // not already moving on the same axis (pressing left while moving
+  // left won't do anything, and pressing right while moving left
+  // shouldn't let you collide with your own body)
+  
+  // left arrow key
+  if (e.which === 37 && snake.dx === 0) {
+    snake.dx = -grid;
+    snake.dy = 0;
+  }
+  // up arrow key
+  else if (e.which === 38 && snake.dy === 0) {
+    snake.dy = -grid;
+    snake.dx = 0;
+  }
+  // right arrow key
+  else if (e.which === 39 && snake.dx === 0) {
+    snake.dx = grid;
+    snake.dy = 0;
+  }
+  // down arrow key
+  else if (e.which === 40 && snake.dy === 0) {
+    snake.dy = grid;
+    snake.dx = 0;
+  }
+});
+
+// start the game
+requestAnimationFrame(loop);
+
+
+var shake = function (element, magnitude = 16, angular = false) {
+  //First set the initial tilt angle to the right (+1) 
+  var tiltAngle = 1;
+
+  //A counter to count the number of shakes
+  var counter = 1;
+
+  //The total number of shakes (there will be 1 shake per frame)
+  var numberOfShakes = 15;
+
+  //Capture the element's position and angle so you can
+  //restore them after the shaking has finished
+  var startX = 0,
+      startY = 0,
+      startAngle = 0;
+
+  // Divide the magnitude into 10 units so that you can 
+  // reduce the amount of shake by 10 percent each frame
+  var magnitudeUnit = magnitude / numberOfShakes;
+
+  //The `randomInt` helper function
+  var randomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  //Add the element to the `shakingElements` array if it
+  //isn't already there
+  if(shakingElements.indexOf(element) === -1) {
+    //console.log("added")
+    shakingElements.push(element);
+
+    //Add an `updateShake` method to the element.
+    //The `updateShake` method will be called each frame
+    //in the game loop. The shake effect type can be either
+    //up and down (x/y shaking) or angular (rotational shaking).
+    if(angular) {
+      angularShake();
+    } else {
+      upAndDownShake();
+    }
+  }
+
+  //The `upAndDownShake` function
+  function upAndDownShake() {
+
+    //Shake the element while the `counter` is less than 
+    //the `numberOfShakes`
+    if (counter < numberOfShakes) {
+
+      //Reset the element's position at the start of each shake
+      element.style.transform = 'translate(' + startX + 'px, ' + startY + 'px)';
+
+      //Reduce the magnitude
+      magnitude -= magnitudeUnit;
+
+      //Randomly change the element's position
+      var randomX = randomInt(-magnitude, magnitude);
+      var randomY = randomInt(-magnitude, magnitude);
+
+      element.style.transform = 'translate(' + randomX + 'px, ' + randomY + 'px)';
+
+      //Add 1 to the counter
+      counter += 1;
+
+      requestAnimationFrame(upAndDownShake);
+    }
+
+    //When the shaking is finished, restore the element to its original 
+    //position and remove it from the `shakingElements` array
+    if (counter >= numberOfShakes) {
+      element.style.transform = 'translate(' + startX + ', ' + startY + ')';
+      shakingElements.splice(shakingElements.indexOf(element), 1);
+    }
+  }
+
+  //The `angularShake` function
+  function angularShake() {
+    if (counter < numberOfShakes) {
+      console.log(tiltAngle);
+      //Reset the element's rotation
+      element.style.transform = 'rotate(' + startAngle + 'deg)';
+
+      //Reduce the magnitude
+      magnitude -= magnitudeUnit;
+
+      //Rotate the element left or right, depending on the direction,
+      //by an amount in radians that matches the magnitude
+      var angle = Number(magnitude * tiltAngle).toFixed(2);
+      console.log(angle);
+      element.style.transform = 'rotate(' + angle + 'deg)';
+      counter += 1;
+
+      //Reverse the tilt angle so that the element is tilted
+      //in the opposite direction for the next shake
+      tiltAngle *= -1;
+
+      requestAnimationFrame(angularShake);
+    }
+
+    //When the shaking is finished, reset the element's angle and
+    //remove it from the `shakingElements` array
+    if (counter >= numberOfShakes) {
+      element.style.transform = 'rotate(' + startAngle + 'deg)';
+      shakingElements.splice(shakingElements.indexOf(element), 1);
+      //console.log("removed")
+    }
+  }
+
+};
+
+// document.querySelector('.red').addEventListener('mouseenter', (e) => {
+//   shake(e.currentTarget);
+// });
+
+// document.querySelector('.green').addEventListener('mouseenter', (e) => {
+//   shake(e.currentTarget, 20, true);
+// });
+
+
+
